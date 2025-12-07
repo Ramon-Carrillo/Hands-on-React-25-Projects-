9d474a03f48972a6ce5a110a34f3b23eac20530e
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2025-12-06 22:45:14 -0600

    added file and updated json setting

---
+++ 01-Fundamentals/tutorial/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+const Greeting = () => {
+  return <h2>Greeting</h2>
+}
+export default Greeting
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Greeting />)
