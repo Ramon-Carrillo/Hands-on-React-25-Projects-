bdbdf5a2e207fecd3942fc9a3f807f63cc944b06
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2025-12-17 16:56:51 -0600

    refactored code for the booklist project

--- 01-Fundamentals/tutorial/src/index.js
+++ 01-Fundamentals/tutorial/src/index.js
@@ -2,37 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const firstBook = {
+  author: 'Emma Greene',
+  title: 'How to Draw Everything',
+  img: './images/book-1.jpg',
+}
+const secondBook = {
+  author: 'Jack Haynes',
+  title: 'Learn a Lot While You Sit on the Pot',
+  img: './images/book-2.jpg',
+}
+const thirdBook = {
+  author: 'Mel Robbins',
+  title: 'The Let Them Theory',
+  img: './images/book-3.jpg',
+}
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/613znXUaEWL._AC_UL600_SR600,400_.jpg'
-    alt='How to Draw Everything'
-  />
-)
-const Title = () => {
-  return <h2>How to Draw Everything</h2>
-}
-const Author = () => <h4>Emma Greene</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
