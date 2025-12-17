8a6c141ebcb38e184510ebf3b55f2ea023e98131
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2025-12-16 23:27:30 -0600

    added a read me file and started book list project.

--- 01-Fundamentals/tutorial/src/index.js
+++ 01-Fundamentals/tutorial/src/index.js
@@ -1,10 +1,39 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const Greeting = () => {
-  return <h2>Greeting</h2>
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
-export default Greeting
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/613znXUaEWL._AC_UL600_SR600,400_.jpg'
+    alt='How to Draw Everything'
+  />
+)
+const Title = () => {
+  return <h2>How to Draw Everything</h2>
+}
+const Author = () => <h4>Emma Greene</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
