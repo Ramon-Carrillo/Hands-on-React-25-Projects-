e085a04ccb4bd8b25ad7daba5be71929fe8e1c64
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-01-09 17:15:41 -0600

    finised the useState section and added fundamental-projects with first project

--- 03-Advanced_React/src/App.jsx
+++ 03-Advanced_React/src/App.jsx
@@ -1,6 +1,6 @@
 // import Starter from './tutorial/01-useState/starter/01-error-example'
 // import Starter from './tutorial/01-useState/starter/02-useState-basics'
-import Starter from './tutorial/01-useState/starter/03-useState-array'
+import Starter from './tutorial/01-useState/starter/05-useState-gotcha'
 
 function App() {
   return (
--- 03-Advanced_React/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-Advanced_React/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,24 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'read books',
+  })
 
-export default UseStateObject;
+  const displayPerson = () => {
+    setPerson({ name: 'jon', age: 28, hobby: 'scream at the computer' })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys to: {person.hobby}</h4>
+      <button className='btn' onClick={displayPerson}>
+        Show jon
+      </button>
+    </>
+  )
+}
+export default UseStateObject
