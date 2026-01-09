8e11a6bd04e67b2b12054d9c9f07a9b005f5ceca
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-01-09 00:26:13 -0600

    finished useState array

--- 03-Advanced_React/src/App.jsx
+++ 03-Advanced_React/src/App.jsx
@@ -1,5 +1,7 @@
 // import Starter from './tutorial/01-useState/starter/01-error-example'
-import Starter from './tutorial/01-useState/starter/02-useState-basics'
+// import Starter from './tutorial/01-useState/starter/02-useState-basics'
+import Starter from './tutorial/01-useState/starter/03-useState-array'
+
 function App() {
   return (
     <div className='container'>
--- 03-Advanced_React/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-Advanced_React/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+import { data } from '../../../data'
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  const [people, setPeople] = useState(data)
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+  const clearAllItems = () => {
+    setPeople([])
+  }
+  return (
+    <div>
+      {people.map(({ id, name }) => {
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type='button' onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+        )
+      })}
+      <button
+        className='btn'
+        type='button'
+        style={{ marginTop: '2rem' }}
+        onClick={clearAllItems}>
+        clear items
+      </button>
+    </div>
+  )
+}
 
-export default UseStateArray;
+export default UseStateArray
