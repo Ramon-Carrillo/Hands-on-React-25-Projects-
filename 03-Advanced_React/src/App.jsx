81f6324b5d061e7e04cce2c21d1f5b50083be266
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-01-19 23:29:05 -0600

    finished project-structure

--- 03-Advanced_React/src/App.jsx
+++ 03-Advanced_React/src/App.jsx
@@ -5,7 +5,7 @@
 // import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
 // import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
 // import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
+// import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 // import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
 // import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 // import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule'
@@ -13,13 +13,10 @@ import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 // import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
 // import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
 // import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+// import Navbar from './tutorial/04-project-structure/starter/Navbar'
 
 function App() {
-  return (
-    <div className='container'>
-      <Starter />
-    </div>
-  )
+  return <div className='container'></div>
 }
 
 export default App
