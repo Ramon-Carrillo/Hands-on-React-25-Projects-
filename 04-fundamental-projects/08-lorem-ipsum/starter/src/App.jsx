cb4fcb4394c318a8fc3a132d4ba5335e738dbfd1
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-01-28 13:08:57 -0600

    finished 08-lorem-ipsum in 04-fundamental-projects

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,45 @@
+import { useState } from 'react'
+import data from './data'
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
-};
-export default App;
+  const [count, setCount] = useState(1)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if (amount < 1) {
+      amount = 1
+    }
+    if (amount > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount))
+  }
+  return (
+    <section className='section-center'>
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+          min='1'
+          max='8'
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
+}
+export default App
