9374694c1f6834fb73716075874d8a42988c58cb
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-02-04 10:04:28 -0600

    finished 12-sidebar in 04-fundamental-projects

---
+++ 04-fundamental-projects/12-sidebar/starter/src/Modal.jsx
@@ -0,0 +1,21 @@
+import { useGlobalContext } from './context'
+import { FaTimes } from 'react-icons/fa'
+
+const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext()
+  if (!isModalOpen) {
+    return null
+  }
+
+  return (
+    <div className='modal-overlay show-modal'>
+      <div className='modal-container'>
+        <h3>modal content</h3>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
+}
+export default Modal
