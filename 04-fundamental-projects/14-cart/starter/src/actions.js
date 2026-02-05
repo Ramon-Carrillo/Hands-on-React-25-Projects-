988d2af33b664d9bdce09c4b5ea628e0411ef63f
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-02-05 15:04:28 -0600

    finished 14-cart in 04-fundamental-projects

---
+++ 04-fundamental-projects/14-cart/starter/src/actions.js
@@ -0,0 +1,7 @@
+export const CLEAR_CART = 'CLEAR_CART'
+export const REMOVE_ITEM = 'REMOVE_ITEM'
+export const INCREASE = 'INCREASE'
+export const DECREASE = 'DECREASE'
+export const LOADING = 'LOADING'
+export const DISPLAY_ITEMS = 'DISPLAY_ITEMS'
+export const TOGGLE_AMOUNT = 'TOGGLE_AMOUNT'
---
+++ 04-fundamental-projects/14-cart/starter/src/context.jsx
@@ -0,0 +1,70 @@
+import { useContext, createContext, useReducer, useEffect } from 'react'
+import reducer from './reducer'
+import cartItems from './data'
+import {
+  CLEAR_CART,
+  REMOVE_ITEM,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+  TOGGLE_AMOUNT,
+} from './actions'
+import { getTotals } from './utils'
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
+
+const AppContext = createContext()
+
+const initialState = {
+  loading: false,
+  cart: new Map(),
+}
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { totalAmount, totalCost } = getTotals(state.cart)
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } })
+  }
+
+  const increaseItem = (id) => {
+    dispatch({ type: INCREASE, payload: { id } })
+  }
+
+  const decreaseItem = (id) => {
+    dispatch({ type: DECREASE, payload: { id } })
+  }
+
+  const fetchData = async () => {
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        removeItem,
+        increaseItem,
+        decreaseItem,
+        totalAmount,
+        totalCost,
+      }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
