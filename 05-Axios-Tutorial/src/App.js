ca543682ed1091572f45857a786e72f7e6716b68
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-02-10 11:42:55 -0600

    finished 01 and 02 in the 05-axios-tutorial

---
+++ 05-Axios-Tutorial/src/App.js
@@ -0,0 +1,14 @@
+// import Setup from './examples/1-first-request'
+import Setup from './examples/2-headers'
+
+import Title from './components/Title'
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  )
+}
+
+export default App
