a53b821dad1c378454d5fc7234cd45f2a20df374
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2026-02-24 13:03:30 -0600

    add axios custom instance and interceptors for API requests

--- 05-Axios-Tutorial/src/App.js
+++ 05-Axios-Tutorial/src/App.js
@@ -1,9 +1,10 @@
 // import Setup from './examples/1-first-request'
 // import Setup from './examples/2-headers'
 // import Setup from './examples/3-post-request'
+// import Setup from './examples/4-global-instance'
+// import Setup from './examples/5-custom-instance'
+import Setup from './examples/6-interceptors'
 import Title from './components/Title'
-import Setup from './examples/4-global-instance'
-import './axios/global'
 function App() {
   return (
     <main>
